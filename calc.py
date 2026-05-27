2057ef74baaaafe3400b288fde8771533e3ff966
Lo-cunha <lorenzomcunha@gmail.com>
2026-05-27 20:12:48 -0300

    first commit

---
+++ calc.py
@@ -0,0 +1,10 @@
+
+while True:
+    print("\n=== Calculadora ===")
+    print("1 - Soma")
+    print("2 - Subtração")
+    print("3 - Multiplicação")
+    print("4 - Divisão")
+    print("0 - Sair")
+
+    opcao = input("Escolha uma opção: ")
