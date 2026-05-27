05af34d40c3176f944e5e9e6b4730e238f5efad3
Lo-cunha <lorenzomcunha@gmail.com>
2026-05-27 20:25:50 -0300

    feat: add sum

--- calc.py
+++ calc.py
@@ -8,3 +8,8 @@ while True:
     print("0 - Sair")
 
     opcao = input("Escolha uma opção: ")
+    if opcao == '1':
+        num1 = float(input("Digite o primeiro número: "))
+        num2 = float(input("Digite o segundo número: "))
+        resultado = num1 + num2
+        print(f"O resultado da soma é: {resultado}")
