f39a33b1144f8464512c08f1f919046d5313bcea
Lo-cunha <lorenzomcunha@gmail.com>
2026-05-27 21:23:40 -0300

    feat: outputs

--- calc.py
+++ calc.py
@@ -1,4 +1,19 @@
-
+def div ():
+    a = int(input("Digite o primeiro número: "))
+    b = int(input("Digite o segundo número: "))
+    print(a/b)
+def mult ():
+    a = int(input("Digite o primeiro número: "))
+    b = int(input("Digite o segundo número: "))
+    print(a*b)
+def soma ():
+    a = int(input("Digite o primeiro número: "))
+    b = int(input("Digite o segundo número: "))
+    print(a+b)
+def sub ():
+    a = int(input("Digite o primeiro número: "))
+    b = int(input("Digite o segundo número: "))
+    print(a-b)
 while True:
     print("\n=== Calculadora ===")
     print("1 - Soma")
@@ -8,3 +23,16 @@ while True:
     print("0 - Sair")
 
     opcao = input("Escolha uma opção: ")
+    if opcao == "1":
+        soma()
+    elif opcao == "2":
+        sub()
+    elif opcao == "3":
+        mult()
+    elif opcao == "4":
+        div()
+    elif opcao == "0":
+        print("Saindo...")
+        break
+    else:
+        print("Opção inválida. Tente novamente.")
